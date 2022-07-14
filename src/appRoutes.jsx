435b366110f32db6a774d289b8d4a021d2e8b487
elias areta <eliasarata@gmail.com>
2022-07-14 18:50:51 +0300

    asd

--- src/appRoutes.jsx
+++ src/appRoutes.jsx
@@ -3,18 +3,7 @@ import React, { useState } from 'react'
 import { useEffect } from 'react';
 
 export default function AppRoutes() {
-
-    useEffect(() =>{
-    doApi();
-    },[]);
-
-    const doApi = async() =>{
-       let url = "/hours.json";
-       let resp = await fetch(url);
-       let data = await resp.json();
-       console.log(data);
-      
-      }
+   
 
   return (
     <div>AppRoutes</div>
