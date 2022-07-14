07a097dc0b343b974e15fc3c328b1635e63873f0
elias areta <eliasarata@gmail.com>
2022-07-14 19:23:59 +0300

    asd

--- src/appRoutes.jsx
+++ src/appRoutes.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import About from './comps/about'
+import Home from './comps/home'
+import Page404 from './comps/page404'
 
 export default function AppRoutes() {
    
 
   return (
-    <div>AppRoutes</div>
+    <BrowserRouter>
+    <Routes>
+    <Route path="/" element={<Home/>}/>
+    <Route path="/about" element={<About/>}/>
+    <Route path="/*" element={<Page404/>}/>
+    </Routes>
+    </BrowserRouter>
   )
 }
