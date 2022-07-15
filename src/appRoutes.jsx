581856e0edc101dd0e906fdc9add65fc605b9b17
elias areta <eliasarata@gmail.com>
2022-07-15 16:52:08 +0300

    asd

--- src/appRoutes.jsx
+++ src/appRoutes.jsx
@@ -4,6 +4,7 @@ import LoginAdmin from './admin_comps/loginAdmin'
 import About from './comps/about'
 import Home from './comps/home'
 import Page404 from './comps/page404'
+import SignUp from './comps/signUp'
 
 export default function AppRoutes() {
 
@@ -12,7 +13,7 @@ export default function AppRoutes() {
     <Routes>
     <Route path="/" element={<Home/>}/>
     <Route path="/about" element={<About/>}/>
-    <Route path="/admin" element={<LoginAdmin/>}/>
+    <Route path="/login" element={<LoginAdmin/>}/>
     <Route path="/*" element={<Page404/>}/>
     </Routes>
     </BrowserRouter>
