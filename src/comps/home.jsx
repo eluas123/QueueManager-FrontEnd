4ca6e1b8665cc6cc1ba3edb5824a74d95262d5f6
elias areta <eliasarata@gmail.com>
2022-07-16 15:04:44 +0300

    שדג

--- src/comps/home.jsx
+++ src/comps/home.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
+import { Link, useNavigate } from 'react-router-dom'
 
 export default function Home() {
+
+  const nav = useNavigate();
+
   return (
-    <div className='container bg-dark'>
-     <h1 className='text-white'>Queue Manager</h1>
+    <div className='container bg-dark h-100'>
+     <h1 className='text-white text-center'>Queue Manager</h1>
      <div className='container'>
-       <h3 className='text-white'>Click  to Turn</h3>
+       <h3 className='text-white text-center mt-5'>Home Page</h3>
+       <Link className='btn btn-success' to={'/login'}>Click</Link>
      </div>
     </div>
   )
