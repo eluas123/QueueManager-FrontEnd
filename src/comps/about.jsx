bcabfdde28dfda4e150d82f9cbb56960e4396d4d
elias areta <eliasarata@gmail.com>
2022-08-15 03:20:41 +0300

    asd

--- src/comps/about.jsx
+++ src/comps/about.jsx
@@ -1,16 +1,31 @@
+import Footer from './footer';
 import React from 'react'
+import { useState } from 'react'
 import HeaderClient from './headerClient'
 
 
-export default function About() {
-
+export default function About(props) {
  
+  let [show,setShow] = useState(true);
+
+  const hideButton = () =>{
+    setShow(false);
+  }
+
+ const backButton = () =>{
+  setShow(true);
+ }
+
   return (
     <React.Fragment>
       <HeaderClient/>
     <div className='container-fluid bg-dark'>
      <h1 className='text-white text-center'>Queue Manager</h1>
        <h3 className='text-white text-center mt-5'>About Page</h3>
+       <button onClick={()=>{
+            {backButton()}
+          }} className='btn btn-success'>back</button>
+       {show && <Footer hideButton={hideButton} txt='elias agever'/>}
        </div>
        </React.Fragment>
   )
--- src/comps/footer.jsx
+++ src/comps/footer.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 
-export default function Footer() {
+export default function Footer(props) {
   return (
     <footer className='container-fluid headerNav'>
        <div className='container text-center text-white pt-4'>
           <strong className=''>copright Elias Areta and Timor Rabani 2022</strong>
+          <h2>{props.txt}</h2>
+          <button onClick={()=>{
+            {props.hideButton()}
+          }} className='btn btn-danger'>none</button>
+          
        </div>
     </footer>
   )
