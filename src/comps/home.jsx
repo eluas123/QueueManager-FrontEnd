90d4f97b40fb022f915c78a69e0060be43cabce4
elias areta <eliasarata@gmail.com>
2022-11-09 00:35:51 +0200

    done

--- src/comps/home.jsx
+++ src/comps/home.jsx
@@ -7,18 +7,42 @@ import { FcCalendar, FcContacts, FcReading, FcSms } from 'react-icons/fc';
 import { MdSmsFailed, MdProductionQuantityLimits } from 'react-icons/md';
 import '..//css//home.css';
 import Footer from './footer';
+import { useState } from 'react';
+import { useContext } from 'react';
+import { AppContext } from '../context/context';
+import { useEffect } from 'react';
+import { TOKEN_NAME } from '../services/apiService';
 
 export default function Home() {
 
+  const {user} = useContext(AppContext);
+  const [show,setShow] = useState(false);
+
+   useEffect(() =>{
+    if(!localStorage[TOKEN_NAME]){
+      setShow(true);
+    }else{
+      setShow(false);
+    }  
+   },[])
+
+  console.log("check",user)
 
   return (
     <React.Fragment>
       <HeaderClient />
       <div className='container-fluid homePage'>
       <div className='container'>
+        {show?
+        <div>
         <h1 className='text-center mt-5 display-4'>ברוכים הבאים למספרה של טימור ואליאס</h1>
         <Link className='btn btn-primary btnHomePageDesktop mt-5' to={"/login"}>בואו נתחיל התחבר/הרשם</Link>
         <Link className='btn btn-primary form-control btnHomePageResponsive mt-5' to={"/login"}>בואו נתחיל התחבר/הרשם</Link>
+        </div>:
+        <div>
+          <h1 className='text-center mt-5 display-4'><strong>{user.name}</strong> שמחים שחזרת אלינו</h1>
+          <div className='mt-5'></div>
+          </div>}
           <div className='row d-flex justify-content-center mt-4'>
             <div className='col-md-6 d-flex justify-content-evenly'>
               <a target={"_blank"} href='https://api.whatsapp.com/send/?phone=972548173179&text=Elias+agever&type=phone_number&app_absent=0'>
@@ -81,15 +105,3 @@ export default function Home() {
     </React.Fragment>
   )
 }
-
-{/* <div className='container-fluid bg-dark'>
-     <h1 className='text-white text-center'>Queue Manager</h1>
-       <h3 className='text-white text-center mt-5'>Home Page</h3>
-       <div className='container d-flex justify-content-evenly mt-5'>
-       <Link className='btn btn-success' to={'/whatsapp'}>WhatsApp</Link>
-       <Link className='btn btn-success' to={'/about'}>About</Link>
-       <Link className='btn btn-success' to={'/service'}>appoitments</Link>
-       <Link className='btn btn-success' to={'/products'}>Products</Link>
-       <Link className='btn btn-success' to={'/waze'}>Waze</Link>
-     </div>
-    </div> */}
